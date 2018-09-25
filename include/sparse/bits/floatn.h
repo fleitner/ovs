fleitner/ovs

commit 07aec2ac19930b05370d813ff5a44e20c2908498

sparse: Support newer GCC/glibc versions.
This fixes some "sparse" errors I encountered after upgrading.

Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Justin Pettit <jpettit@ovn.org>

include/sparse/bits/floatn.h
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2018 Nicira, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __CHECKER__
+#error "Use this header only with sparse.  It is not a correct implementation."
+#endif
+
+#ifndef __BITS_FLOATN_SPARSE
+#define __BITS_FLOATN_SPARSE 1
+
+/* "sparse" claims to be a recent version of GCC but doesn't support IEEE 754
+ * binary128, so we define macros to make that clear. */
+
+#define __HAVE_FLOAT128 0
+#define __HAVE_FLOAT64X 0
+
+#include <bits/floatn-common.h>
+
+#endif /* <bits/floatn.h> for sparse */

include/sparse/automake.mk
@@ -1,5 +1,6 @@
 noinst_HEADERS += \
         include/sparse/arpa/inet.h \
+        include/sparse/bits/floatn.h \
         include/sparse/assert.h \
         include/sparse/bmi2intrin.h \
         include/sparse/emmintrin.h \
@@ -13,5 +14,6 @@ noinst_HEADERS += \
         include/sparse/rte_memcpy.h \
         include/sparse/rte_vect.h \
         include/sparse/sys/socket.h \
+        include/sparse/sys/sysmacros.h \
         include/sparse/sys/types.h \
         include/sparse/sys/wait.h

include/sparse/sys/sysmacros.h
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2018 Nicira, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __CHECKER__
+#error "Use this header only with sparse.  It is not a correct implementation."
+#endif
+
+#ifndef __SYS_SYSMACROS_SPARSE
+#define __SYS_SYSMACROS_SPARSE 1
+
+/* "sparse" doesn't like the large constants in <bits/sysmacros.h>, complaining
+ * that they are so large that they have type "unsigned long long".  This
+ * header avoids the problem. */
+
+unsigned int major(dev_t);
+unsigned int minor(dev_t);
+dev_t makedev(unsigned int, unsigned int);
+
+#endif /* <sys/sysmacros.h> for sparse */

commit 0d45b97b235f7f9f9ab282e12e5ec27efcb64c3e

sparse: check if floatn-common.h is available.
This skip including floatn-common.h if it's not available since it
was introduced in glibc 2.27 and OVS doesn't not actually require
that to work with previous glibc version.

Fixes: 07aec2ac1 sparse: Support newer GCC/glibc versions.
Signed-off-by: Flavio Leitner <fbl@sysclose.org>

configure.ac
@@ -110,7 +110,7 @@ AC_CHECK_MEMBERS([struct sockaddr_in6.sin6_scope_id], [], [],
 #include <netinet/in.h>]])
 AC_CHECK_FUNCS([mlockall strnlen getloadavg statvfs getmntent_r sendmmsg clock_gettime])
 AC_CHECK_HEADERS([mntent.h sys/statvfs.h linux/types.h linux/if_ether.h])
-AC_CHECK_HEADERS([linux/net_namespace.h stdatomic.h])
+AC_CHECK_HEADERS([linux/net_namespace.h stdatomic.h bits/floatn-common.h])
 AC_CHECK_HEADERS([net/if_mib.h], [], [], [[#include <sys/types.h>
 #include <net/if.h>]])
 

include/sparse/bits/floatn.h
@@ -27,6 +27,9 @@
 #define __HAVE_FLOAT128 0
 #define __HAVE_FLOAT64X 0
 
+#ifdef HAVE_BITS_FLOATN_COMMON_H
+/* Introduced in glibc 2.27 */
 #include <bits/floatn-common.h>
+#endif
 
 #endif /* <bits/floatn.h> for sparse */