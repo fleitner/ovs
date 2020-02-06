fleitner/ovs

commit d4877184856088be8ddb038bbca23a934bb5bd2a

dp-packet: preserve headroom when cloning a pkt batch
The headroom is useful if the packet needs to insert additional
header, so preserve the original headroom when cloning the batch.

Signed-off-by: Flavio Leitner <fbl@sysclose.org>
Tested-by: Ciara Loftus <ciara.loftus.intel.com>
Acked-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: Ian Stokes <ian.stokes@intel.com>

lib/dp-packet.h
@@ -879,7 +879,11 @@ dp_packet_batch_clone(struct dp_packet_batch *dst,
 
     dp_packet_batch_init(dst);
     DP_PACKET_BATCH_FOR_EACH (i, packet, src) {
-        dp_packet_batch_add(dst, dp_packet_clone(packet));
+        uint32_t headroom = dp_packet_headroom(packet);
+        struct dp_packet *pkt_clone;
+
+        pkt_clone  = dp_packet_clone_with_headroom(packet, headroom);
+        dp_packet_batch_add(dst, pkt_clone);
     }
     dst->trunc = src->trunc;
 }

commit 1371bcbc1e67a3f405c1265ea11bd4ac45bc236f

dp-packet: prefetch data_off when cloning a batch.
There is a cache miss while cloning a batch and using prefetch
improved the throughput by 2.7%.

Fixes: d48771848560 ("dp-packet: preserve headroom when cloning a pkt batch")
Signed-off-by: Flavio Leitner <fbl@sysclose.org>

lib/dp-packet.h
@@ -495,6 +495,13 @@ dp_packet_set_size(struct dp_packet *b, uint32_t v)
                                       * this segment. */
 }
 
+/* Prefetch data_off from packet 'b'. */
+static inline void
+__packet_data_prefetch(const struct dp_packet *b)
+{
+    OVS_PREFETCH(&(b->mbuf.data_off));
+}
+
 static inline uint16_t
 __packet_data(const struct dp_packet *b)
 {
@@ -622,6 +629,12 @@ static inline void
 dp_packet_set_size(struct dp_packet *b, uint32_t v)
 {
     b->size_ = v;
+}
+
+/* Only available for DPDK. */
+static inline void
+__packet_data_prefetch(const struct dp_packet *b)
+{
 }
 
 static inline uint16_t
@@ -879,6 +892,10 @@ dp_packet_batch_clone(struct dp_packet_batch *dst,
 
     dp_packet_batch_init(dst);
     DP_PACKET_BATCH_FOR_EACH (i, packet, src) {
+        if ((i + 1) < dp_packet_batch_size(src)) {
+            __packet_data_prefetch(src->packets[i+1]);
+        }
+
         uint32_t headroom = dp_packet_headroom(packet);
         struct dp_packet *pkt_clone;
 