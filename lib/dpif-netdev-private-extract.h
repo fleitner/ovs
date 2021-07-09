15b3884fc4c00520b4bf5d0dbab7e792fc72df42
Harry van Haaren <harry.van.haaren@intel.com>
2021-07-09 17:36:00 +0530

    dpif-netdev/mfex: Add AVX512 based optimized miniflow extract
    This commit adds AVX512 implementations of miniflow extract.
    By using the 64 bytes available in an AVX512 register, it is
    possible to convert a packet to a miniflow data-structure in
    a small quantity instructions.
    
    The implementation here probes for Ether()/IP()/UDP() traffic,
    and builds the appropriate miniflow data-structure for packets
    that match the probe.
    
    The implementation here is auto-validated by the miniflow
    extract autovalidator, hence its correctness can be easily
    tested and verified.
    
    Note that this commit is designed to easily allow addition of new
    traffic profiles in a scalable way, without code duplication for
    each traffic profile.
    
    Signed-off-by: Harry van Haaren <harry.van.haaren@intel.com>

--- lib/dpif-netdev-private-extract.h
+++ lib/dpif-netdev-private-extract.h
@@ -77,14 +77,15 @@ enum dpif_miniflow_extract_impl_idx {
     MFEX_IMPL_AUTOVALIDATOR,
     MFEX_IMPL_SCALAR,
     MFEX_IMPL_STUDY,
+    MFEX_IMPL_VMBI_IPv4_UDP,
+    MFEX_IMPL_IPv4_UDP,
     MFEX_IMPL_MAX
 };
 
 /* Define a index which points to the first traffic optimized MFEX
  * option from the enum list else holds max value.
  */
-
-#define MFEX_IMPL_START_IDX MFEX_IMPL_MAX
+#define MFEX_IMPL_START_IDX MFEX_IMPL_VMBI_IPv4_UDP
 
 /* This function returns all available implementations to the caller. The
  * quantity of implementations is returned by the int return value.
@@ -154,5 +155,25 @@ mfex_study_traffic(struct dp_packet_batch *packets,
  */
 uint32_t mfex_set_study_pkt_cnt(uint32_t pkt_cmp_count,
                                 const char *name);
+
+/* AVX512 MFEX Probe and Implementations functions. */
+#ifdef __x86_64__
+int32_t mfex_avx512_probe(void);
+int32_t mfex_avx512_vbmi_probe(void);
+
+#define DECLARE_AVX512_MFEX_PROTOTYPE(name)                                 \
+    uint32_t                                                                \
+    mfex_avx512_vbmi_##name(struct dp_packet_batch *packets,                \
+                            struct netdev_flow_key *keys, uint32_t keys_size,\
+                            odp_port_t in_port, struct dp_netdev_pmd_thread \
+                            *pmd_handle);                                   \
+    uint32_t                                                                \
+    mfex_avx512_##name(struct dp_packet_batch *packets,                     \
+                        struct netdev_flow_key *keys, uint32_t keys_size,   \
+                        odp_port_t in_port, struct dp_netdev_pmd_thread     \
+                        *pmd_handle);                                       \
+
+DECLARE_AVX512_MFEX_PROTOTYPE(ip_udp);
+#endif /* __x86_64__ */
 
 #endif /* MFEX_AVX512_EXTRACT */
